5b2bbc358165ce4d573f2f52584467a997d9a522
sanjana <147934397+sanju131103@users.noreply.github.com>
2024-10-06 13:39:42 +0800

    tournament management

---
+++ FenceLink/src/main/java/com/example/tournament/TournamentService.java
@@ -0,0 +1,29 @@
+package com.example.tournament;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TournamentService {
+
+    @Autowired
+    private TournamentRepository tournamentRepository;
+
+    public void addTournament(Tournament tournament) {
+        tournamentRepository.addTournament(tournament);
+    }
+
+    public void updateTournament(Tournament tournament) {
+        tournamentRepository.updateTournament(tournament);
+    }
+
+    public void deleteTournament(Long id) {
+        tournamentRepository.deleteTournament(id);
+    }
+
+    public List<Tournament> getAllTournaments() {
+        return tournamentRepository.getAllTournaments();
+    }
+}
